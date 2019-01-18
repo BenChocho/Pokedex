1a4013226ecc04083817161cfd4b8967dc3f8aa2
BenChocho <35330748+BenChocho@users.noreply.github.com>
2019-01-18 15:05:12 +0100

    Remove Modal + Style Input

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,56 +1,57 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import pokeLoad from '../pokeLoad.gif';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-class Pokemon extends React.Component {
+class PokemonSearch extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
-            imageUrl: '',
-            imageLoading: true,
-            toManyRequests: false,
-            pokemonIndex: '',
-            modalUrl: `#modal${this.props.name}`,
-            modal: false,
+            url: 'https://pokeapi.co/api/v2/pokemon/',
+            pokemonData: null,
+            count: 80,
+            input: '',
+            matchingPokemon: ''
         }
     }
 
     async componentDidMount() {
-        const name = this.props.name;
-        const url = this.props.url;
-        const pokemonIndex = url.split('/')[url.split('/').length -2];
-        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
+        const resp = await axios.get(this.state.url);
+        this.setState({pokemonData: resp.data['results']});
+    }
 
-        this.setState({name, imageUrl, pokemonIndex})
+    addMoreData() {
+      if (this.state.count < 949) {
+        this.setState({count: this.state.count + 80});
+      }
+    }
+
+    searchPokemon(e) {
+        this.setState({input: e.target.value});
+        const matchingPokemon = this.state.pokemonData.filter(
+            b => b.name.toLowerCase().trim().indexOf(this.state.input) > -1)
+            .map(c => c.name);
+        this.setState({ matchingPokemon})
+        console.log(this.state.matchingPokemon)
     }
 
     render() {
-
         return (
-            <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
-                    {this.state.imageLoading ? ( <img alt="loader" src={pokeLoad} className="loader"/>) : null}
-                    <img 
-                    className="pokemonImg"
-                    alt={this.state.name}
-                    src={this.state.imageUrl} 
-                    onLoad={() => this.setState({imageLoading: false})}
-                    onError={() => this.setState({toManyRequests: true})}
-                    style={this.state.toManyRequests ? { display : "none" } : this.state.imageLoading ? null : {display : "block"}}
-                    />
-                    <div className="card-body mx-auto">
-                        <h1 className="card-title clrWhite">{this.state.name}</h1>
-                        <p className="text-center clrWhite">ID #{this.state.pokemonIndex}</p>
-                        <Link to={`pokemon/${this.state.pokemonIndex}`}>
-                            <Button color="danger">Stats</Button>
-                        </Link>
-                    </div>
+            <div>
+                <input className="form-control" type="text" value={this.state.input} onChange={this.searchPokemon.bind(this)} placeholder="Search"/>
+                <div>
+                <div>
+                    {this.state.matchingPokemon.length
+                    ? 
+                        (<div>{this.state.matchingPokemon
+                        .map(p => (<Link key={p} to={`pokemon/${p}`}><p key={p}>{p}</p></Link>))}
+                        </div>) 
+                    : 
+                        (<h1 className="clrWhite">Waiting for search</h1>)}
+                </div>
                 </div>
             </div>
         );
     }
 }
 
-export default Pokemon
+export default PokemonSearch;
